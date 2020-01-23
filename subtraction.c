438c69050b1165023bf713240441ed8e4de5dff7
Demir Khawaja <khawajad7828@my.sduhsd.net>
2020-01-23 12:25:04 -0800

    Final Project

---
+++ subtraction.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int areaOfCircle(int smaller, int larger) 
+{
+  int solution = (smaller - larger);
+  return solution;
+}
+
+
+int main(int argc, char* argv[])
+{
+  
+  if (argc != 3)
+  {
+    printf("%s : expected 2 args, please enter two integers\n", argv[0]);
+    return 1;
+  }
+
+  
+ int smaller;
+  
+  int found = sscanf(argv[1], "%d", &smaller);
+  if (found != 1)
+  {
+    printf("first arg is not an integer, enter two ints\n");
+    return 1;
+  }
+ int larger;
+  found = sscanf(argv[2], "%d", &larger);
+  if (found != 1)
+  {
+    printf("second arg is not an integer, enter two ints\n");
+    return 1;
+  }
+
+
+int solution = areaOfCircle(smaller, larger);
+printf("The solution is %d\n", solution);
+
+}
+
